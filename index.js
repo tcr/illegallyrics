3e52e254fb10e9324fced07067fc236e5342b3f3
Tim Cameron Ryan <id@timryan.org>
2014-08-16 01:38:28 -0700

    More information! More power!

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var skim = require('skim');
 
 function steal (search, next) {
   rem.stream('http://lyrics.wikia.com/Special:Search', {
-    search: search
+    search: search,
+    fulltext: 'Search',
+    ns0: 1,
+    ns220: 1,
   }).get().pipe(skim({
     results: {
       $query: 'article h1 a',
@@ -14,15 +17,30 @@ function steal (search, next) {
       return url.substr(10).indexOf(':') > -1;
     })[0];
 
+    if (!likelyurl) {
+      return next(new Error('No pages found.'), 'No pages found.');
+    }
+
     rem.stream(likelyurl).get().pipe(skim({
+      'title': {
+        $query: '#WikiaPageHeader h1',
+        $value: '(text)',
+      },
       'lyrics': {
         $query: '.lyricbox',
         $value: '(html)',
       }
     }, function (data) {
-      var html = data.lyrics;
+      var html = (data || {}).lyrics || 'No lyrics found.';
       var lyrics = html.replace(/\n\n[\s\S]+$/, '').replace(/^[\s\S]+Ad/, '').replace(/<br>/g, '\n').replace(/<[^>]+>/g, '');
-      next(null, lyrics);
+
+      var artist = ((data || {}).title || '').replace(/:.*$/, '');
+      var title = ((data || {}).title || '').replace(/^.*?:/, '').replace(/\s*Lyrics\s*$/i, '');
+      next(null, {
+        artist: artist,
+        title: title,
+        lyrics: lyrics
+      });
     }))
   }));
 }
